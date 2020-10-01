3d238cb1ef2231ad9558860bc6a96cb65ae56136
CodeX20 <simrann2099@gmail.com>
2020-10-01 15:42:20 +0530

    Create Array-Intersection.cpp

---
+++ C++ Practicals/Array-Intersection.cpp
@@ -0,0 +1,49 @@
+/This code is used to find the array intersection elements using STL implemented hashmaps
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include<unordered_map>
+using namespace std;
+
+// input1 - first array
+// input2 - second array
+// size1 - size of first array
+// size2 - size of second array
+void intersection(int input1[], int input2[], int size1, int size2) {
+     unordered_map<int,int>m;
+  int i;
+  for( i=0;i<size1;i++)
+  {
+    int key=input1[i];
+      m[key]++;   
+  }
+  for(int j=0;j<size2;j++)
+  {
+    if(m[input2[j]]>0)
+    {
+        cout<<input2[j]<<'\n';
+        m[input2[j]]--;
+    } 
+}
+       }
+int main() {
+
+	int size1,size2;
+
+	cin>>size1;
+	int *input1=new int[1+size1];	
+	
+	for(int i=0;i<size1;i++)
+		cin>>input1[i];
+
+	cin>>size2;
+	int *input2=new int[1+size2];	
+	
+	for(int i=0;i<size2;i++)
+		cin>>input2[i];
+	
+	
+	intersection(input1,input2,size1,size2);
+		
+	return 0;
+}
